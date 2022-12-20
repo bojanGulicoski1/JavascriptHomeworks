280a9561518829fb2485d7a9f5e1de1aa86c837d
bojanGulicoski1 <119902954+bojanGulicoski1@users.noreply.github.com>
2022-12-20 21:37:42 +0100

    homework
    homework1

---
+++ js.homework1/app.js
@@ -0,0 +1,9 @@
+//homework 1 tellStory
+function tellStorry(array){
+ 
+    let story =`Hello this is ${array[0]} hes very ${array[1]} and hes ${array[2]} all day every day! `;
+    return story
+  }
+    let result =tellStorry(["Bojan" , "mad" , "boxing"]);
+    console.log(result);
+  
